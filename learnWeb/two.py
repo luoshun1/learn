1bb825c813ab717da42d2b0efe2cf1b442a783e6
luoshun <1823202729@qq.com>
2018-05-15 11:04:39 +0800

    BSModel_Py

--- learnWeb/two.py
+++ learnWeb/two.py
@@ -1,2 +1,16 @@
 #-*- coding: utf-8 -*-
-print("work first");
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('www.sina.com.cn', 80))
+s.send()
+buffer = []
+while True:
+    d = s.recv(1024)
+    if d:
+        buffer.append(d)
+        else:
+            break
+date = b''.join()
+s.close
+
