8c77173a4a8d2b56f43a5aa00d17c868c834f9fd
luoshun <1823202729@qq.com>
2018-05-26 18:04:36 +0800

    test

--- learnWeb/delete.py
+++ learnWeb/delete.py
@@ -2,13 +2,13 @@
 import os
 print("hahah")
 
-root_path = 'D:\work\env1\client\AppPlay'
+root_path = 'D:\\work\\env1\\client\\AppPlay'
 fs = os.listdir(root_path)
 for f1 in fs:
 	print(f1)
-	tag_path = os.path.join(root_path, f1, 'jni\Android.mk')
-	if os.path.isfile(tag_path):
-		os.remove(tag_path);
+	tag_file = os.path.join(root_path, f1, 'jni\\Android.mk')
+	if os.path.isfile(tag_file):
+		os.remove(tag_file)
 #test
 # root_path = 'D:\myCode\\test\\test'
 # fs = os.listdir(root_path)
